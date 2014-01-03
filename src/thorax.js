17d415cb9eb42c24f4476789242fe09c80fd5ac9
kpdecker <kpdecker@gmail.com>
2014-01-03 14:43:05 -0600

    Add serverSide client failover

--- src/thorax.js
+++ src/thorax.js
@@ -1,4 +1,9 @@
-/*global cloneInheritVars, createInheritVars, resetInheritVars, createRegistryWrapper, getValue, inheritVars, createErrorMessage */
+/*global $serverSide, assignTemplate, createErrorMessage, createInheritVars, createRegistryWrapper, getValue, inheritVars, resetInheritVars */
+
+// Provide default behavior for client-failover
+if (typeof $serverSide === 'undefined') {
+  window.$serverSide = false;
+}
 
 //support zepto.forEach on jQuery
 if (!$.fn.forEach) {
